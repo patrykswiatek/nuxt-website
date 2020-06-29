8d4c38450c8f6165911539ab3b00392be10392c5
Patrick <patryk.swiatek@monterail.com>
2020-06-29 19:40:24 +0200

    add development tools

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.container {
+  background-color: $havelock-blue;
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
